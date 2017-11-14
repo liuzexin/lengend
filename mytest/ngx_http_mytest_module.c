b99505fdba8da0b507a63c5db17e8db99707756a
liuzexin <10848224@qq.com>
2017-11-14 09:53:37 +0800

    modified:   mytest/ngx_http_mytest_module.c

--- mytest/ngx_http_mytest_module.c
+++ mytest/ngx_http_mytest_module.c
@@ -9,7 +9,7 @@
 //static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r);
 
 static char * ngx_http_mytest(ngx_conf_t *cf, nginx_command_t*cmd, void *conf);
-static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t * r);
+//////////////////static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t * r);
 
 static nginx_command_t ngx_http_mytest_commands[] = {
 	{ngx_string("mytest"),
