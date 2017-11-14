dd4bf773512f26768d22221b084d74096c8b9929
CoderLiu <liuzexin_top@163.com>
2017-11-14 09:52:09 +0800

    fixedissue

--- mytest/ngx_http_mytest_module.c
+++ mytest/ngx_http_mytest_module.c
@@ -53,7 +53,7 @@ ngx_module_t ngx_http_mytest_module = {
 	NGX_MODULE_V1_PADDING 
 };
 
-static char * ngx_http_mytest(ngx_conf_t *cf, nginx_command_t*cmd, void *conf){
+static char* ngx_http_mytest(ngx_conf_t *cf, nginx_command_t*cmd, void *conf){
 	ngx_http_core_loc_conf_t *clcf;
 	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
 	clcf->handler = ngx_http_mytest_handler;
@@ -62,6 +62,6 @@ static char * ngx_http_mytest(ngx_conf_t *cf, nginx_command_t*cmd, void *conf){
 
 static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t * r){
 	ngx_log_error(NGX_LOG_DEBUG_HTTP, r->connection->log, 0,
-                   "http mytest: \"%s\"", "Test");
-	return NGX_CONF_OK;
+                   "http mytest: \"%s\"", (char *)"Test");
+	return NGX_DONE;
 }
